3402619749525a9a0e194dfbb8382aa7630623fe
grace <graceogwe@gmail.com>
2024-05-14 11:30:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App(){
+  return(
+    <div>
+      <h1 style={{color:"dodgerblue",position:"relative",left:"200px",bottom:"100px"}}>This is just a Test Website</h1>
+    </div>
+  )
+}
